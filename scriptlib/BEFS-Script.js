7660756083ebf58b356ef4e24179adacedaecc0b
Geo_mzn <geomznplays@gmail.com>
2026-06-04 02:34:35 -0700

    Update BEFS-Script.js

--- scriptlib/BEFS-Script.js
+++ scriptlib/BEFS-Script.js
@@ -15,21 +15,6 @@ if (!input || !cs) {
 // input and console stuff
 
 input.placeholder = "Input command here...";
-input.style.cssText = `
-position: absolute;
-left: 0px;
-bottom: 0px;
-right: 0px;
-border-width: 2px 0px 0px;
-border-style: solid none none;
-border-color: white;
-background: #0f172a;
-z-index: 255;
-color: white;
-height: 1rem;
-opacity: 1;
-display: block;
-`;
 cs.style.cssText = `background: #0f172a;
 font-family: sans-serif;
 z-index: 254;
